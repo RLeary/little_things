71171613463a9053ec85bc26e1fc8502ffa13245
Ruaraidh <ruaraidh.leary@clyde.space>
2019-07-18 15:50:49 +0100

    base conversions py

--- Python/base_conversions.py
+++ Python/base_conversions.py
@@ -7,6 +7,11 @@ def dec_to_hex(number):
 def dec_to_oct(number):
 	return "{0:o}".format(number)
 	
-# Quick and dirty - change later
+# Quick and dirty for next 2 - change later
 def bin_to_dec(number):
 	return int(number, 2)
+	
+def bin_to_hex(number):
+	return hex(bin_to_dec(number))
+	
+	
