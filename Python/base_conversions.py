e60d797e1b16d59dce999b8181cf02e0b69d4535
Ruaraidh <ruaraidh.leary@clyde.space>
2019-07-18 15:49:31 +0100

    unit conversions py

---
+++ Python/base_conversions.py
@@ -0,0 +1,12 @@
+def dec_to_bin(number):
+	return "{0:b}".format(number)
+	
+def dec_to_hex(number):
+	return "{0:x}".format(number)
+	
+def dec_to_oct(number):
+	return "{0:o}".format(number)
+	
+# Quick and dirty - change later
+def bin_to_dec(number):
+	return int(number, 2)
